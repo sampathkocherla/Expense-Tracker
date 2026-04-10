ec6f6722a63ad1342a8dcb7cdac5c9677c5ed7db
sampath <sampathkocherla7@gmail.com>
2026-04-10 18:41:36 +0530

    deployment added

--- backend/server.js
+++ backend/server.js
@@ -33,4 +33,4 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+ module.exports = app;
